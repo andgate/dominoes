75e86660edbf649555b6bf39c897bb1879a3805a
Matt Harker <mattsharker@gmail.com>
2018-04-22 21:13:08 -0700

    Adds documentation to table.h

--- include/table.h
+++ include/table.h
@@ -16,12 +16,14 @@ using namespace std;
 
 class Table
 {
+
 private:
 	static const int DOMINO_MAX = 6;
 	static const int DOMINOS_COUNT = 26;
 
 	vector<shared_ptr<Domino>> 	boneyard;
 	list<shared_ptr<Domino>> 	chain;
+
 
 public:
 	Table()
@@ -32,9 +34,15 @@ public:
 	~Table()
 	{}
 
+	/**
+	 * @brief Creates the dominoes
+	 * 
+	 * Generates every domino possible and adds
+	 * them to the boneyard. Then randomizes the
+	 * order of the boneyard
+	 */
 	void setup()
 	{
-		// Generate dominos
 		boneyard.clear();
 		boneyard.reserve(DOMINOS_COUNT);
 		for (int i = 0; i <= DOMINO_MAX; ++i) {
@@ -48,6 +56,15 @@ public:
     	shuffle(boneyard.begin(), boneyard.end(), default_random_engine(seed));
 	}
 
+
+	/**
+	 * @brief Adds a domino into the player's hand
+	 * 		
+	 * 	If there are dominoes left in the boneyard, it will
+	 * 	move one from it into the player's hand
+	 * 
+	 * @return shared_ptr<Domino> 
+	 */
 	shared_ptr<Domino> draw()
 	{
 		if (boneyard.empty()) return nullptr;
@@ -56,34 +73,71 @@ public:
 		return dom;
 	}
 
+	/**
+	 * @brief Determines if the boneyard is empty
+	 * 
+	 * @return true 
+	 * @return false 
+	 */
 	bool boneyardEmpty()
 	{
 		return boneyard.empty();
 	}
 
+	/**
+	 * @brief Get the Chain Head object
+	 * 
+	 * @return int 
+	 */
 	int getChainHead()
 	{
 		return chain.front()->getHead();
 	}
 
+	/**
+	 * @brief Adds the domino to the head of the chain
+	 * 
+	 * @param dom 
+	 */
 	void addChainHead(shared_ptr<Domino> dom)
 	{
 		chain.push_front(dom);
 	}
 
+	/**
+	 * @brief Get the Chain Tail object
+	 * 
+	 * @return int 
+	 */
 	int getChainTail()
 	{
 		return chain.back()->getTail();
 	}
 
+	/**
+	 * @brief Adds the domino to the tail of the chain
+	 * 
+	 * @param dom 
+	 */
 	void addChainTail(shared_ptr<Domino> dom)
 	{
 		chain.push_back(dom);
 	}
 
-
+	/**
+	 * @brief Tries to play a domino to the chain
+	 * 
+	 * Takes the current domino and checks the to see if it can
+	 * be played on the chain in its current rotation. If it can't
+	 * then flip it and check again.
+	 * 
+	 * @param dom 
+	 * @return true 
+	 * @return false 
+	 */
 	bool playPiece(shared_ptr<Domino> dom)
 	{
+		
 		if(chain.empty())
 		{
 			chain.push_back(dom);
@@ -115,7 +169,10 @@ public:
 		return false;
 	}
 
-
+	/**
+	 * @brief Prints the current boneyard
+	 * 
+	 */
 	void printBoneyard()
 	{
 		for (auto& dom : boneyard)
@@ -123,7 +180,10 @@ public:
 		cout << endl;
 	}
 
-
+	/**
+	 * @brief Prints the current domino chain
+	 * 
+	 */
 	void printChain()
 	{
 		for (auto& dom : chain)
